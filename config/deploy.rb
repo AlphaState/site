8074f375d0152d9c4a6aa7e86f9c5dfa21b9542f
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-04-04 14:48:52 +0200

    Capify!

---
+++ config/deploy.rb
@@ -0,0 +1,24 @@
+lock '3.1.0'
+
+set :application, 'alpha'
+set :repo_url, 'gitolite@ukhov.me:web/alpha.git'
+
+namespace :deploy do
+  desc 'Configure the application'
+  task :configure do
+    on roles(:app) do
+      execute "ln -s #{ deploy_to }/shared/config/database.yml #{ release_path }/config/database.yml"
+    end
+  end
+
+  desc 'Restart the application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :finishing, 'deploy:cleanup'
+  after :finishing, 'configure'
+  after :finishing, 'restart'
+end
