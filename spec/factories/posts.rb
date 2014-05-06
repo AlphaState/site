5f561b798c6a614dcc5ec28b2daf3a289bf422ba
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-05-06 20:16:25 +0200

    Removed ./test; split spec/factories.rb into two (posts and projects)

---
+++ spec/factories/posts.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  sequence :post_address do |n|
+    "post_#{ n }"
+  end
+
+  factory :post do
+    title 'Birds and bees'
+    author 'Ivan'
+    description 'About birds and bees'
+    address { generate :post_address }
+    content 'Once upon a time…'
+  end
+end
--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,16 +1,4 @@
 FactoryGirl.define do
-  sequence :post_address do |n|
-    "post_#{ n }"
-  end
-
-  factory :post do
-    title 'Birds and bees'
-    author 'Ivan'
-    description 'About birds and bees'
-    address { generate :post_address }
-    content 'Once upon a time…'
-  end
-
   sequence :project_address do |n|
     "project_#{ n }"
   end
@@ -22,7 +10,7 @@ FactoryGirl.define do
   factory :project do
     title 'Rocket to the Moon'
     motto 'Beautiful things work beautifully'
-    thumbnail '/favicon.png'
+    thumbnail '/project.png'
     address { generate :project_address }
     priority { generate :project_priority }
     content 'Let’s rock!'
