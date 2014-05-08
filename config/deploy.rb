f5038e7abd0d489a13668a76e1b00c83e90dd9ed
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-05-08 22:47:33 +0200

    Changed the log level of the production environment

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,14 +6,14 @@ set :repo_url, 'gitolite@ukhov.me:web/alpha.git'
 namespace :deploy do
   desc 'Configure the application'
   task :configure do
-    on roles(:app) do
+    on roles(:all) do
       execute "ln -s #{ deploy_to }/shared/config/database.yml #{ release_path }/config/database.yml"
     end
   end
 
   desc 'Restart the application'
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
+    on roles(:all), in: :sequence, wait: 5 do
       execute :touch, release_path.join('tmp/restart.txt')
     end
   end
