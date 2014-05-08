50c0ca4b26a7f5e512814d1ccb46755dc7dc104c
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-05-08 06:38:13 +0200

    Removed the unsecure attr_accessor :password_digest from the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
   validates :name, :email, presence: true
   validates :email, uniqueness: true
 
-  attr_accessor :password_digest
   has_secure_password validations: false
   validates :password, presence: true, on: :create
 end
