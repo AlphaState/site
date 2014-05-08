fadcb84def5701220537f939240db6bc6157da47
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-05-08 20:47:30 +0200

    Made the authentication features pass

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -1,6 +1,12 @@
 Given /am a (\w+)/ do |role|
   case role
   when 'user'
+    user = create :user
+    visit signin_path locale: I18n.locale
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Sign in'
+    expect(page).to have_content('Sign out')
   when 'visitor'
   else
     raise "Cannot recognize the role '#{ role }'."
