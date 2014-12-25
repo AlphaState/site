1bfc9c792bc13672769ad135ca43e961b0e709ae
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-12-25 09:09:20 +0100

    Updated deploy/production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,12 +6,13 @@ server 'alphastate.se', user: 'ivan', roles: %w{web app}
 set :deploy_to, '/home/ivan/projects/alpha'
 
 paths = [
+  '/usr/local/rvm/rubies/ruby-2.1.5',
   '/usr/local/rvm/gems/ruby-2.1.5',
   '/usr/local/rvm/gems/ruby-2.1.5@global',
 ]
 
 set :default_env, {
-  GEM_HOME: paths[0],
-  GEM_PATH: paths.join(':'),
+  GEM_HOME: '/usr/local/rvm/gems/ruby-2.1.5',
+  GEM_PATH: '/usr/local/rvm/gems/ruby-2.1.5:/usr/local/rvm/gems/ruby-2.1.5@global',
   PATH: "#{paths.map{ |p| File.join(p, 'bin') }.join(':')}:$PATH",
 }
