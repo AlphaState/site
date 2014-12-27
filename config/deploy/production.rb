a54b4ebce93e87e939a4df726964f997121f59e5
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-12-27 10:24:33 +0100

    Changed the address of the server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 set :stage, :production
 
-role :web, 'ivan@alphastate.se'
-server 'alphastate.se', user: 'ivan', roles: %w{web app}
+role :web, 'ivan@site.alphastate.se'
+server 'site.alphastate.se', user: 'ivan', roles: %w{web app}
 
 set :deploy_to, '/home/ivan/projects/alpha-v2'
 
