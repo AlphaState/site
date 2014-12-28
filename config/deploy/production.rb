5e377084bb3d01f8d0352440af8af85a3f7b21bc
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-12-28 09:07:35 +0100

    Adjusted the deployment configuration

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,18 +1,18 @@
 set :stage, :production
 
-role :web, 'ivan@alphastate.se'
-server 'alphastate.se', user: 'ivan', roles: %w{web app}
+role :web, 'ivan@site.alphastate.se'
+server 'site.alphastate.se', user: 'ivan', roles: %w{web app}
 
 set :deploy_to, '/home/ivan/projects/alpha-v1'
 
 paths = [
-  '/usr/local/rvm/rubies/ruby-2.1.5',
-  '/usr/local/rvm/gems/ruby-2.1.5',
-  '/usr/local/rvm/gems/ruby-2.1.5@global',
+  '/usr/local/rvm/rubies/ruby-2.2.0',
+  '/usr/local/rvm/gems/ruby-2.2.0',
+  '/usr/local/rvm/gems/ruby-2.2.0@global',
 ]
 
 set :default_env, {
-  GEM_HOME: '/usr/local/rvm/gems/ruby-2.1.5',
-  GEM_PATH: '/usr/local/rvm/gems/ruby-2.1.5:/usr/local/rvm/gems/ruby-2.1.5@global',
+  GEM_HOME: '/usr/local/rvm/gems/ruby-2.2.0',
+  GEM_PATH: '/usr/local/rvm/gems/ruby-2.2.0:/usr/local/rvm/gems/ruby-2.2.0@global',
   PATH: "#{paths.map{ |p| File.join(p, 'bin') }.join(':')}:$PATH",
 }
